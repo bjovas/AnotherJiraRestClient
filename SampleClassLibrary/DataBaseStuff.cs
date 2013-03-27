1d8900e9f24d08983ed4007950a3feba2b07f44f
Bjørn Otto Vasbotten <bjorn@pswin.com>
2013-03-27 21:51:08 +0100

    Added a sample class lib to make stacktraces that little bit more realistic.

---
+++ SampleClassLibrary/DataBaseStuff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace AspnetSampleApp
+{
+    public class DataBaseStuff
+    {
+        public int GetUserIdFromUsername(string username)
+        {
+            // In this method we would read to from our database. Let us fake a dead connection
+ 
+            SqlConnection conn = new SqlConnection("Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;");
+            conn.Open();
+
+            // do some query
+
+            conn.Close();
+
+            return 11443; // this will of course never happen.
+        }
+    }
+}
---
+++ SampleClassLibrary/HeavyCalculations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleClassLibrary
+{
+    public class HeavyCalculations
+    {
+        public List<int> DoSomeHeavyStuff(int arg1, int arg2, int arg3)
+        {
+            
+            // Lets calculate doing something that runs out of memory
+            throw new OutOfMemoryException("This is a fake exception created at " + DateTime.Now.ToShortTimeString());
+
+
+        }
+    }
+}
